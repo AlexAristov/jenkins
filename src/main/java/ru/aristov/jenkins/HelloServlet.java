4f0e903f71232915772344067318e1d24951375d
AlexAristov <asaristov@yandex.ru>
2024-06-21 10:42:26 +0300

    Update HelloServlet.java
    8

--- src/main/java/ru/aristov/jenkins/HelloServlet.java
+++ src/main/java/ru/aristov/jenkins/HelloServlet.java
@@ -14,6 +14,6 @@ public class HelloServlet extends HttpServlet {
         response.setCharacterEncoding("UTF-8");
         PrintWriter writer = response.getWriter();
 
-        writer.write("Hello 7");
+        writer.write("Hello 8");
     }
 }
