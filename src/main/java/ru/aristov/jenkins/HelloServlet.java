2adaff62d77f7a75dc13e37677e6101b79cf2de9
Alex <asaristov@yandex.ru>
2024-06-21 19:30:44 +0300

    fix: hello message

--- src/main/java/ru/aristov/jenkins/HelloServlet.java
+++ src/main/java/ru/aristov/jenkins/HelloServlet.java
@@ -14,6 +14,6 @@ public class HelloServlet extends HttpServlet {
         response.setCharacterEncoding("UTF-8");
         PrintWriter writer = response.getWriter();
 
-        writer.write("Hello 12");
+        writer.write("Hello 13");
     }
 }
